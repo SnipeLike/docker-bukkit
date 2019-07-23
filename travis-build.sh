83ce4d5458c07c2e55124ecd1e7fbf2f3e147691
Bren Briggs <code@fraq.io>
2019-07-23 00:56:29 -0400

    Tell docker what to build
    Signed-off-by: Bren Briggs <code@fraq.io>

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # Build
-docker build --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG
+docker build --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
 
 # Test
 docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bbriggs/bukkit:$TAG
